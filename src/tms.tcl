dec130b5d81de55ef8de06266c377615bf65e844
Ryan Davis <iconoclasmandheresy@gmail.com>
2014-10-06 16:25:38 -0700

    OO switchover started. OO connection done.

--- src/tms.tcl
+++ src/tms.tcl
@@ -11,28 +11,87 @@ set serverport 5432
 #
 # End of configuration. Don't change below here.
 #
+package require TclOO
+
+oo::class create connection {
+   constructor {chanid clientaddress clientport} {
+      my variable channel
+      my variable address
+      my variable port
+      my variable active
+      my variable tag
+      my variable owner
+      set owner nil
+      set channel $chanid
+      set address $clientaddress
+      set port $clientport
+      set active True
+      set tag "$address:$port"
+      fconfigure $channel -blocking False -buffering line -translation auto
+      fileevent $channel readable [list [self] newline ]
+      puts "$tag ! connected"
+      my echo "$tag ! connected"
+   }
+   destructor {
+      my variable channel
+      fileevent $channel readable {}
+      close $channel
+   }
+# Getter methods
+   method channel {} { my variable channel ; return $channel }
+   method address {} { my variable address ; return $address }
+   method port {} { my variable port ; return $port }
+   method active {} { my variable active ; return $active }
+   mrthod tag {} { my variable tag ; return $tag }
+# Setter Methods
+   method settag {newtag} {
+      my variable tag
+      set tag $newtag
+      return $tag
+   }
+# I/O
+   method echo {str} {
+   # Send string argument to client
+   my variable active
+   my variable channel
+   my variable tag
+      if {$active} {
+         puts $channel $str
+         puts "$tag > $str"
+      }
+   }
+   method readline {} {
+   # Return line read from client
+   my variable active
+   my variable channel
+   my variable tag
+      if {$active} {
+         if {[gets $channel line] >= 0} {
+            puts "$tag < $line"
+            return $line
+         }
+         if {[eof $channel]} {
+            puts "$tag ! closing"
+            close $channel
+            set active False
+         }
+      } { return "" }
+   }
+   method newline {} {
+   # triggered on readable input
+   # Currently a stup to simply echo.
+      my echo [my readline]
+   }
+   export echo address port active tag settag
+}
 
 set pendingconnects {}
 set forever 0
 set characters(.connectcount) 0
 
 proc newconnection {chanid clientaddr clientport} {
-   puts "Newconnect: $clientaddr"
-   puts $chanid "Newconnect: $clientaddr"
-   lappend pendingconnects $chanid
-   fconfigure $chanid -blocking False -buffering line -translation auto
-   fileevent $chanid readable [list testEcho $chanid]
-}
-
-proc testEcho {chanid} {
-   if {[gets $chanid line] >= 0} {
-      puts $line
-      puts $chanid $line
-   }
-   if {[eof $chanid]} {
-      puts closing
-      close $chanid
-   }
+   global pendingconnects
+   lappend pendingconnects [connection new $chanid $clientaddr $clientport]
 }
 
 socket -server newconnection $serverport
